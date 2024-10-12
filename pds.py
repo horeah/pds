92ca64afacb28d2f645d44456cd021a5607a89a2
Horea Haitonic <horeah@gmail.com>
2024-10-12 18:00:39 +0300

    Use argparse to parse the "mode" argument

--- pds.py
+++ pds.py
@@ -1,11 +1,14 @@
 import sys
 import os
 import pickle
+import argparse
 
 import pathlib
 
-mode = sys.argv[1]
-expr = sys.argv[2]
+parser = argparse.ArgumentParser(prog='pds')
+parser.add_argument('mode', choices=('none', 'each', 'filter', 'iter'))
+parser.add_argument('expression')
+args = parser.parse_args(sys.argv[1:])
 
 def iterator():
     while True:
@@ -19,25 +22,25 @@ it = iterator()
 
 output = print if os.isatty(sys.stdout.fileno()) else lambda o: pickle.dump(o, sys.stdout.buffer)
 
-match mode:
-    case '--none':
-        r = eval(expr, {'pathlib': pathlib})
+match args.mode:
+    case 'none':
+        r = eval(args.expression, {'pathlib': pathlib})
         if hasattr(r, '__next__'):
             for e in r:
                 output(e)
         else:
             output(r)
-    case '--each':
+    case 'each':
         for x in it:
-            y = eval(expr, {'pathlib': pathlib}, {'x': x})
+            y = eval(args.expression, {'pathlib': pathlib}, {'x': x})
             output(y)
-    case '--filter':
+    case 'filter':
         for x in it:
-            f = eval(expr, {'pathlib': pathlib}, {'x': x})
+            f = eval(args.expression, {'pathlib': pathlib}, {'x': x})
             if f:
                 output(x)
-    case '--iter':
-        r = eval(expr, {'it': it})
+    case 'iter':
+        r = eval(args.expression, {'it': it})
         if hasattr(r, '__next__'):
             for e in r:
                 output(e)
