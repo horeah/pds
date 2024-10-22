3599fc80dc61bb072c6d437e31c057362f2952f1
Horea Haitonic <horeah@gmail.com>
2024-10-22 22:31:13 +0300

    New "source" script pds-files producing a stream of pathlib.Path objects

---
+++ pds-files.py
@@ -0,0 +1,21 @@
+import sys
+import pickle
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser(prog='pds-files')
+parser.add_argument('path', nargs='*', default='.')
+parser.add_argument('-R', '--recursive', action='store_true')
+args = parser.parse_args()
+
+if sys.platform == 'win32':
+    args.path = [g for p in args.path for g in (Path('.').glob(p) if p != '.' else p)]
+                                         
+for p in args.path:
+    p = Path(p)
+    expression = f'pathlib.Path("{p}")'
+    if p.is_dir():
+        pattern = '**/*' if args.recursive else '*'
+        expression += f'.glob("{pattern}")'
+    sys.argv = ['pds.py', 'none', expression]
+    exec(open('pds.py').read(), globals(), {})
