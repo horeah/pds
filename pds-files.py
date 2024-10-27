31a8631e539a843226dbbb5e31bd54065a63125c
Horea Haitonic <horeah@gmail.com>
2024-10-27 16:40:02 +0200

    Rewritten pds-files source script to account for Win vs. Linux
    The way the glob expansion is treated on the command line is
    completely different across systems:
    
    In Linux, the shell expands the wildcards (incl. recursive wildcards
    for modern shells!), then the expanded array gets passed to the
    spawned process as argv
    
    In Windows, the glob argument is passed to the spawned process as it
    is, and it the responsibility of the process to do the expansion.

--- pds-files.py
+++ pds-files.py
@@ -2,20 +2,36 @@ import sys
 import pickle
 import argparse
 from pathlib import Path
+from itertools import chain
 
 parser = argparse.ArgumentParser(prog='pds-files')
-parser.add_argument('path', nargs='*', default='.')
+parser.add_argument('path', nargs='*', default=[Path()], type=Path)
 parser.add_argument('-R', '--recursive', action='store_true')
 args = parser.parse_args()
 
+paths = iter(())
 if sys.platform == 'win32':
-    args.path = [g for p in args.path for g in (Path('.').glob(p) if p != '.' else p)]
-                                         
-for p in args.path:
-    p = Path(p)
-    expression = f'pathlib.Path("{p}")'
-    if p.is_dir():
-        pattern = '**/*' if args.recursive else '*'
-        expression += f'.glob("{pattern}")'
-    sys.argv = ['pds.py', 'none', expression]
-    exec(open(Path(__file__).parent / 'pds.py').read(), globals(), {})
+    for path in args.path:
+        if '*' in str(path) or '?' in str(path):
+            if path.is_absolute():
+                anchor = Path(path.anchor)
+                paths = chain(paths,
+                              (anchor / p for p in anchor.glob('/'.join(path.parts[1:]))))
+            else:
+                paths = chain(paths, (p for p in Path().glob(str(path))))
+        else:
+            if path.is_dir():
+                paths = chain(paths, (p for p in path.glob('**/*' if args.recursive else '*')))
+            else:
+                paths = chain(paths, iter((path,)))
+else:
+    for path in args.path:
+        if path.is_dir():
+            paths = chain(paths, (p for p in path.glob('**/*' if args.recursive else '*')))
+        else:
+            paths = chain(paths, iter((path,)))
+
+pds_code = open(Path(__file__).parent / 'pds.py').read()
+for path in paths:
+    sys.argv = ['pds.py', 'none', f'pathlib.Path("{path.as_posix()}")']
+    exec(pds_code, globals(), {})
--- tests/test_paths.py
+++ tests/test_paths.py
@@ -5,8 +5,9 @@ import pickle
 from pathlib import Path
 from subprocess import Popen, PIPE
 
-def pds_files(args):
-    process = Popen(f'"{sys.executable}" pds-files.py {args}', shell=True, stdout=PIPE)
+def pds_files(*args):
+    args_str = ' '.join([*args])
+    process = Popen(f'"{sys.executable}" pds-files.py {args_str}', shell=True, stdout=PIPE)
     result = []
     while True:
         try:
@@ -23,8 +24,12 @@ class TestPdsPaths(unittest.TestCase):
     def test_paths(self):
         self.assertEqual(pds_files('tests/paths'),
                          list(Path('tests/paths').glob('*')))
-        self.assertEqual(pds_files('tests/paths/*.dat'),
-                         [Path('tests/paths/c.dat')])
+        self.assertEqual(pds_files('tests/paths/*.txt'),
+                         list(Path('tests/paths').glob('*.txt')))
+        self.assertEqual(pds_files('-R', 'tests'),
+                         list(Path('tests').glob('**/*')))
+        self.assertEqual(pds_files(str(Path().absolute())),
+                         list(p.absolute() for p in Path().glob('*')))
                          
         
 
