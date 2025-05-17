0c3048b6b3933c423099b6be909ac45974c7c458
Horea Haitonic <horeah@gmail.com>
2025-05-17 17:31:06 +0300

    Added test for the pds-procs command

---
+++ tests/test_procs.py
@@ -0,0 +1,30 @@
+import unittest
+import sys
+import psutil
+import pickle
+from subprocess import Popen, PIPE
+
+def pds_procs(*args):
+    args_str = ' '.join([*args])
+    process = Popen(f'"{sys.executable}" pds-procs.py {args_str}', shell=True, stdout=PIPE)
+    result = []
+    while True:
+        try:
+            result.append(pickle.load(process.stdout))
+        except EOFError:
+            break
+    process.communicate()
+    return result
+
+class TestPdsProcs(unittest.TestCase):
+    """
+    Integration tests for the pds-procs source script
+    """
+    def test_procs(self):
+        procs = pds_procs()
+        self.assertTrue(psutil.Process() in procs)
+        if sys.platform == 'win32':
+            self.assertEqual(procs[0].pid, 0)
+            self.assertEqual(procs[0].name(), 'System Idle Process')
+        else:
+            self.assertEqual(procs[0].pid, 1)
